495f7dbf91ed4d49f8b98a72ad6d242bebd3b835
SUCCESSSOFTWARE\anh.phan <anh.phan@successsofware.global>
2022-05-25 13:55:40 +0700

    http

--- tour_of_heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour_of_heroes/src/app/hero-detail/hero-detail.component.ts
@@ -7,7 +7,7 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
@@ -24,10 +24,14 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
   }
   goBack(): void {
     this.location.back();
+  }
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
   }
 }
